e789ea671878a1d50df8154f5f60504f27467264
KapilMP <kapilpradhan77ik@gmail.com>
2025-01-08 22:10:33 +0545

    Implemented UnitOfWork

--- BookBazaar.DataAccess/Repository/IRepository/ICategoryRepository.cs
+++ BookBazaar.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -12,7 +12,7 @@ namespace BookBazaar.DataAccess.Repository.IRepository
         //we dont add update method directly in generic repository because when you are
         //updating category logic might be different compared to product entity
         void Update(Category obj);
-        void Save();
+       // void Save();
 
     }
 }
---
+++ BookBazaar.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using BookBazaar.Data;
+using BookBazaar.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBazaar.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        // Property to provide access to the CategoryRepository
+        // It allows other parts of the application to use the CategoryRepository through the
+        // UnitOfWork
+        public ICategoryRepository Category { get; private set; }
+        // The private database context that will be shared across repositories
+        private readonly ApplicationDbContext _db;
+        // Constructor to initialize the UnitOfWork
+        // Takes an ApplicationDbContext instance as a parameter and assigns it to _db
+        // Also initializes the CategoryRepository with the shared database context
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);//_db is passed as parameter
+                                                   //such that CategoryRepository can interact database
+                                                   //now category property of unitofwork can be used anywhere in the application
+
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
